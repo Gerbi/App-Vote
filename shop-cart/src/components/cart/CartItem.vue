9344fb3e68f1e4e349d45988d4db420688221183
Gerbi <gerbi@protonmail.com>
2018-11-15 14:23:04 -0700

    fetch cart items from server

--- shop-cart/src/components/cart/CartItem.vue
+++ shop-cart/src/components/cart/CartItem.vue
@@ -1,18 +1,25 @@
 <template>
     <tr>
-        <td>Learn Node</td>
-        <td><input type="number" class="form-control" min="1" style="width:100px" value="1" /> </td>
-        <td>$10.0</td>
+        <td>{{cartItem.title}}</td>
+        <td>
+            <input type="number" class="form-control" min="1" style="width:100px" :value="cartItem.qty" />
+        </td>
+        <td>${{cartItem.price}}</td>
         <td>
             <button class="btn btn-danger"> Remove</button>
         </td>
     </tr>
-
 </template>
 
 <script>
     export default {
-        name: "CartItem"
+        name: "CartItem",
+        props:{
+            cartItem:{
+                type: Object,
+                required: true
+            }
+        }
     }
 </script>
 
--- shop-cart/src/store/cart/index.js
+++ shop-cart/src/store/cart/index.js
@@ -1,11 +1,27 @@
+import axios from "axios"
+import {API_URL} from "../../constants";
+
 const state = {
     cartItems : []
 };
-const getters = {};
+const getters = {
+    cartItems(state){
+        return state.cartItems;
+    }
+};
 
-const mutations = {};
+const mutations = {
+    UPDATE_CART_ITEMS(state, payload){
+        state.cartItems = payload;
+    }
+};
 
-const actions = {};
+const actions = {
+    async fetchCartItems(context){
+        const {data} = await axios.get(`${API_URL}/cart`);
+        context.commit('UPDATE_CART_ITEMS', data)
+    }
+};
 
 const cartModule = {
     state,
