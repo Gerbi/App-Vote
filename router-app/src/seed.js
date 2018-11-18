b6c1e6a5275178c587687af4dd66e98cfb4dc6b1
Gerbi <gerbi@protonmail.com>
2018-11-17 18:31:37 -0700

    Build Inital Project

---
+++ router-app/src/seed.js
@@ -0,0 +1,17 @@
+export const todos = [
+    {
+        id: '1',
+        title: 'Learn Node',
+        completed: true
+    },
+    {
+        id: '2',
+        title: 'Learn React',
+        completed: false
+    },
+    {
+        id: '3',
+        title: 'Learn Vue 3.0',
+        completed: true
+    }
+]
