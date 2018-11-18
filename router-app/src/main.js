e4fe578110c202d92459c366aea40836e38a2a55
Gerbi <gerbi@protonmail.com>
2018-11-17 21:00:57 -0700

    Build Inital Project

--- router-app/src/main.js
+++ router-app/src/main.js
@@ -7,8 +7,22 @@ import Blog from './components/Blog.vue'
 import Products from './components/Products.vue'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//registrar vue-router
+vue.use(VueRouter);
+
+//definir las routas para la aplicacion
+const router = new VueRouter({
+    base: __dirname,
+    routes:[
+        {path: '/', component: Home},
+        {path: '/products', component: Products},
+        {path: '/blog', component: Blog}
+    ]
+})
 
 new Vue({
+    router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
