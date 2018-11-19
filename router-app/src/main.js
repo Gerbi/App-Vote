e6df0fada3075f8f6582fa53f148d92f4d29b793
Gerbi <gerbi@protonmail.com>
2018-11-19 13:29:29 -0700

    dynamic routing

--- router-app/src/main.js
+++ router-app/src/main.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router';
 import Home from './components/Home.vue'
 import Blog from './components/Blog.vue'
 import Products from './components/Products.vue'
+import ProductDetails from './components/ProductDetails.vue'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false;
@@ -19,6 +20,8 @@ const router = new VueRouter({
     routes:[
         {path: '/', component: Home},
         {path: '/products', component: Products},
+        //agregar ruta del detalle del producto
+        {path: '/products/:id', component: ProductDetails},
         {path: '/blog', component: Blog}
     ]
 });
