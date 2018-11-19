54b2dcfdd91216b4054ee3dfb827d07d89d75294
Gerbi <gerbi@protonmail.com>
2018-11-19 13:33:20 -0700

    dynamic rounting

---
+++ router-app/src/components/ProductDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Product Details</h1>
+        <div class="card">
+            <div class="card-body">
+                <p>Product Id {{$route.params.id}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProductDetails"
+    }
+</script>
